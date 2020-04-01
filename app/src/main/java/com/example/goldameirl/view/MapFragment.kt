4dcc9c6130e62ba80dc2d451002d1707eb398543
Rivka Schuss <rschuss123@gmail.com>
2020-04-01 18:27:24 +0300

    Implemented displaying user's location on Map

--- app/src/main/java/com/example/goldameirl/view/MapFragment.kt
+++ app/src/main/java/com/example/goldameirl/view/MapFragment.kt
@@ -1,27 +1,40 @@
 package com.example.goldameirl.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.goldameirl.R
 import com.example.goldameirl.viewmodel.MapViewModel
 import com.example.goldameirl.databinding.FragmentMapBinding
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 
 /**
  * A simple [Fragment] subclass.
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), PermissionsListener, OnMapReadyCallback {
 
     lateinit var mapView: MapView
 
     lateinit var viewModel: MapViewModel
+
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+    private lateinit var mapboxMap: MapboxMap
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,13 +44,67 @@ class MapFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
         mapView = binding.mapView
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-
-                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
-            }
-        }
+        mapView.getMapAsync(this)
         return binding.root
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            enableLocationComponent(it)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(activity)) {
+
+// Create and customize the LocationComponent's options
+            val customLocationComponentOptions = LocationComponentOptions.builder(activity!!)
+                .trackingGesturesManagement(true)
+                .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(activity!!, loadedMapStyle)
+                .locationComponentOptions(customLocationComponentOptions)
+                .build()
+
+// Get an instance of the LocationComponent and then adjust its settings
+            mapboxMap.locationComponent.apply {
+
+                // Activate the LocationComponent with options
+                activateLocationComponent(locationComponentActivationOptions)
+
+// Enable to make the LocationComponent visible
+                isLocationComponentEnabled = true
+
+// Set the LocationComponent's camera mode
+                cameraMode = CameraMode.TRACKING
+
+// Set the LocationComponent's render mode
+                renderMode = RenderMode.COMPASS
+            }
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(activity)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+        Toast.makeText(activity, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocationComponent(mapboxMap.style!!)
+        } else {
+            Toast.makeText(activity, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show()
+            activity?.finish()
+        }
     }
 
     override fun onResume() {
