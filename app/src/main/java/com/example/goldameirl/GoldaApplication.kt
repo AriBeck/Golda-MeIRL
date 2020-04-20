720812ec2226f53aa5addf51fabcc6fd89d19294
Rivka Schuss <rschuss123@gmail.com>
2020-04-20 18:05:55 +0300

    Implemented daily background branch refresh using WorkManager

---
+++ app/src/main/java/com/example/goldameirl/GoldaApplication.kt
@@ -0,0 +1,47 @@
+package com.example.goldameirl
+
+import android.app.Application
+import android.os.Build
+import android.util.Log
+import androidx.work.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class GoldaApplication: Application() {
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshWorker>(
+            1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+        Log.d("GoldaApplication", "WorkManager: Periodic Work request for sync is scheduled")
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+}
---
+++ app/src/main/java/com/example/goldameirl/RefreshWorker.kt
@@ -0,0 +1,29 @@
+package com.example.goldameirl
+
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.goldameirl.model.BranchRepository
+import retrofit2.HttpException
+
+class RefreshWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "com.example.goldameirl.RefreshWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val repository = BranchRepository(applicationContext)
+
+        try {
+            repository.refreshBranches()
+            Log.d("RefreshWorker", "Refresh request run")
+        } catch (e: HttpException) {
+            return Result.retry()
+        }
+
+        return Result.success()
+    }
+}
