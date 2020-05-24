06e7c0310b0720eb9b0ccac9a42fd31ce4c2aea5
Ari <ari.beck.52@gmail.com>
2020-05-24 17:28:23 +0300

    Made geojson api generic and made constants file

---
+++ app/src/main/java/com/example/goldameirl/misc/Constants.kt
@@ -0,0 +1,23 @@
+package com.example.goldameirl.misc
+
+const val TOKEN =
+    "pk.eyJ1IjoiYXJpYmVjazUyIiwiYSI6ImNrOGZwa2ZveTAxdzQzbG4yOTl5ajZhOWgifQ.Ht_h6D6yCqdRTJohKF0nJA"
+
+const val ALERT_CHANNEL_ID = "com.example.goldameirl.discounts"
+const val ALERT_GROUP_ID = "com.example.goldameirl"
+
+const val DEFAULT_ZOOM = 15.0
+
+const val ANITA_LAYER_ID = "anita-layer"
+const val ANITA_SOURCE_ID = "anita-source"
+const val ANITA_ICON_ID = "anita-icon"
+
+const val BASE_URL =
+    "https://wow-final.firebaseio.com/"
+
+const val DEFAULT_RADIUS = 500
+const val DEFAULT_INTERVAL = 5L
+const val DEFAULT_ALERT_TIME = 0L
+
+const val INTERVAL_KEY = "time"
+const val RADIUS_KEY = "radius"
